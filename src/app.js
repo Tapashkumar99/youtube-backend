c5dc69ac8eab850920344a1a4054e7d0ac21eeb0
tapash kumar <tapashkumar118@gmail.com>
2022-10-09 22:21:52 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 //  Importing Mongoose Schema from subscribers.js
 const susbcriberSchema = require("./models/subscribers");
@@ -25,18 +25,35 @@ app.get("/subscribers/names", async (req, res) => {
 
 //  Creating GET API response with a subscriber with given id. (an object)
 //  and response error with status code 400 if id does not match the format.
-app.get("/subscribers/:id",cors(), async (req, res,next ) => {
+app.get("/subscribers/:id", cors(), async (req, res, next) => {
   await susbcriberSchema.find({ _id: req.params.id }, (error, result) => {
     if (error) {
-      res.status(400).json({ message: "400 bad request", error });
-      next()
+      res.status(400).json({ message: "400 bad request" });
+      next();
     } else if (result.length == 0) {
       res.status(400).json({ message: "400 bad request" });
-      next()
+      next();
     } else {
       res.status(200).json(result);
       next();
     }
+  });
+});
+
+
+// Error Logging
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 400;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
   });
 });
 
